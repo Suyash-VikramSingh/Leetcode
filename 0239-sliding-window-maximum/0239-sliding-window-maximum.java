65f3bfc05fe8b39c99cc4bca1e7107cd2317dcbe
Suyash Vikram Singh <160521132+Suyash-VikramSingh@users.noreply.github.com>
2025-08-13 21:02:55 +0530

    Time: 460 ms (5.01%), Space: 61.2 MB (29.14%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] ans = new int[n-k+1];
+
+        //finding next greatest element
+        Stack<Integer> st = new Stack<>();
+        int[] nge = new int[n];
+        st.push(n-1);
+        nge[n-1] = n;
+        for(int i=n-2; i>=0; i--){
+            while(!st.isEmpty() && nums[i]>nums[st.peek()]) st.pop();
+            if(st.isEmpty()) nge[i] = n;
+            else nge[i] = st.peek();
+            st.push(i);
+        }
+        int j=0, z=0, max;
+        //Traversing window
+        for(int i=0; i<n-k+1; i++){
+            if(j >= i+k) j = i;
+            max = nums[j];
+            while(j<i+k){
+                max = nums[j];
+                j = nge[j];
+            }
+            ans[z++] = max;
+        }
+        return ans;
+    }
+}
